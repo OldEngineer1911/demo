7eb3c90a1f66d7bb2bf0172292b1e167170aceae
Old Engineer <oldengineer1911@gmail.com>
2021-12-19 15:04:33 +0100

    Remove springboottest annotation.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,12 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.codeborne.selenide.Selenide.open;
 
-@SpringBootTest
 class DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
     @Test
     void openGoogle() {
